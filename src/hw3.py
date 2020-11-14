62b19c8ebd35512e9bd827df0366fc008af90c2e
Prakash Dhimal <dhimalp@gmail.com>
2020-11-14 14:08:39 -0500

    adding Manav's model fit fix

--- src/hw3.py
+++ src/hw3.py
@@ -29,8 +29,8 @@ https://www.kaggle.com/jonathanbesomi/cord-19-sources-unification-with-pyspark-s
 
 import glob
 
+from pyspark.ml.clustering import LDA
 from pyspark.ml.feature import CountVectorizer, Tokenizer
-from pyspark.mllib.clustering import LDA
 from pyspark.sql import SparkSession
 from pyspark.sql import functions as F
 from pyspark.sql.functions import monotonically_increasing_id
@@ -87,34 +87,28 @@ def main():
     # Tokenize the text in the text column
     tokenizer = Tokenizer(inputCol="body_text", outputCol="words")
     wordsDataFrame = tokenizer.transform(data_df)
+    wordsDataFrame.toPandas()
 
-    # remove 20 most occuring documents, documents with non numeric characters, and documents with <= 3 characters
-    cv_tmp = CountVectorizer(inputCol="words", outputCol="vectors")
+    # Count vectorizer
+    cv_tmp = CountVectorizer(inputCol="words", outputCol="features")
     cvmodel = cv_tmp.fit(wordsDataFrame)
     df_vect = cvmodel.transform(wordsDataFrame)
 
     # todo - need to add id column here?
     df_vect = df_vect.select("*").withColumn("id", monotonically_increasing_id())
 
-    # sparsevector = df_vect.select('vectors', 'id').rdd.map(parseVectors)
-    # todo - use the parseVectors method above
-    sparsevector = df_vect.select('vectors').rdd.map(list)
-
     # n_components=50, random_state=0
     num_topics = 10
     max_iterations = 50
     # Train the LDA model, set seed?
-    model = LDA.train(sparsevector, k=num_topics, maxIterations=max_iterations)
+    lda = LDA(seed=1, optimizer="em", k=num_topics, maxIter=max_iterations)
+    model = lda.fit(df_vect)
 
-    # Print the topics in the model
-    # todo - make this better, use viz stuff here.
-    topics = model.describeTopics(maxTermsPerTopic=15)
-    for x, topic in enumerate(topics):
-        print('topic nr: ' + str(x))
-        words = topic[0]
-        weights = topic[1]
-        for n in range(len(words)):
-            print(cvmodel.vocabulary[words[n]] + ' ' + str(weights[n]))
+    wordNumbers = 10  # number of words per topic
+    topicIndices = model.describeTopics(maxTermsPerTopic=wordNumbers)
+    topicIndices.show()
+
+    print(model)
 
 
 if __name__ == '__main__':
