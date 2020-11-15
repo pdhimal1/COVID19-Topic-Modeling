28a8391cbc051abb9314ebcb6d2d4fd0836dd4d7
mgarkel <manav.garkel@gmail.com>
2020-11-15 17:31:04 -0500

    Data Cleaning Done

--- src/hw3.py
+++ src/hw3.py
@@ -26,87 +26,51 @@ https://www.kaggle.com/maksimeren/covid-19-literature-clustering
 Reading this data with pyspark - done
 https://www.kaggle.com/jonathanbesomi/cord-19-sources-unification-with-pyspark-sql
 """
-
-import numpy as np # linear algebra
-import pandas as pd # data processing, CSV file I/O (e.g. pd.read_csv)
-import matplotlib.pyplot as plt
-plt.style.use('ggplot')
-import glob
-import json
-
-from pyspark.sql import SparkSession
-from pyspark import SparkConf, SparkContext
-from pyspark.sql import functions as F
-from pyspark.sql.types import StringType
-from pyspark.ml.feature import CountVectorizer, Tokenizer, StopWordsRemover
-from pyspark.ml.clustering import LDA
+import os
 from time import time
 
-start = time()
+from nltk.corpus import stopwords
+from pyspark.ml.clustering import LDA
+from pyspark.ml.feature import CountVectorizer, Tokenizer, StopWordsRemover
+from pyspark.sql import SparkSession
+from pyspark.sql import functions as F
+from pyspark.sql.types import StringType
 
-# Spark setup
-conf = SparkConf().setMaster("local").setAppName("HW3-data-exploration")
-sc = SparkContext(conf=conf)
-spark = SparkSession(sc)
+stop_words = set(stopwords.words('english'))
 
-# Get all json files
-root_path = '../data/archive/'
-all_json = glob.glob(f'{root_path}/**/*.json', recursive=True)
-print("There are ", len(all_json), "sources files.")
-#todo - for now restrict this to 100 files
-all_json = all_json[:500]
 
-data = spark.read.json(all_json, multiLine=True)
-data.createOrReplaceTempView("data")
+def init_spark():
+    spark = SparkSession.builder.appName("HW3-Coord-data").getOrCreate()
+    return spark
 
-# Select text columns
-covid_sql = spark.sql(
+
+def read_json_files(root_path, spark):
+    json_dir = root_path + "document_parses/pdf_json/"
+    filenames = os.listdir(json_dir)
+
+    all_json = [json_dir + filename for filename in filenames]
+    # todo - for now restrict this to 100 files
+    all_json = all_json[:100]
+
+    data = spark.read.json(all_json, multiLine=True)
+    data.createOrReplaceTempView("data")
+    return data
+
+
+def get_body_text(spark, data):
+    # Select text columns
+    # todo - add more columns
+    covid_sql = spark.sql(
         """
-        SELECT
-            metadata.title AS title,
-            body_text.text AS body_text,
-            paper_id
-        FROM data
-        """)
-
-covid_sql = covid_sql.na.drop(subset=["body_text"])
-word_join_f = F.udf(lambda x: [''.join(w) for w in x], StringType())
-covid_sql = covid_sql.withColumn("body_text", word_join_f("body_text"))
-
-# todo - include word count graph here
+            SELECT
+                body_text.text AS body_text,
+                paper_id
+            FROM data
+            """)
+    return covid_sql
 
 
-# Tokenize the text in the text column
-tokenizer = Tokenizer(inputCol="body_text", outputCol="words")
-token_DataFrame = tokenizer.transform(covid_sql)
-
-# Remove stopwords
-remover = StopWordsRemover(inputCol="words", outputCol="filtered")
-cleaned_DataFrame = remover.transform(token_DataFrame)
-
-# Count vectorizer
-cv_tmp = CountVectorizer(inputCol="filtered", outputCol="features")
-cvmodel = cv_tmp.fit(cleaned_DataFrame)
-df_vect = cvmodel.transform(cleaned_DataFrame)
-
-# todo - need to add id column here?
-
-
-# Fit the LDA Model
-num_topics = 10
-max_iterations = 50
-lda = LDA(seed=1, optimizer="em", k=num_topics, maxIter=max_iterations)
-lda_model = lda.fit(df_vect)
-
-# Get terms per topic
-topics = lda_model.topicsMatrix()
-vocabArray = cvmodel.vocabulary
-
-wordNumbers = 15  # number of words per topic
-topicIndices = lda_model.describeTopics(maxTermsPerTopic = wordNumbers).rdd.map(tuple)
-
-
-def topic_render(topic):  # specify vector id of words to actual words
+def topic_render(topic, wordNumbers, vocabArray):  # specify vector id of words to actual words
     terms = topic[1]
     result = []
     for i in range(wordNumbers):
@@ -114,12 +78,67 @@ def topic_render(topic):  # specify vector id of words to actual words
         result.append(term)
     return result
 
-topics_final = topicIndices.map(lambda topic: topic_render(topic)).collect()
 
-for topic in range(len(topics_final)):
-    print ("Topic" + str(topic) + ":")
-    for term in topics_final[topic]:
-        print (term)
-    print ('\n')
+def clean_up_sentences(sentence):
+    matches = [word for word in sentence.split(' ') if word.isalnum()]
+    matches = [word.lower() for word in matches]
+    matches = [word for word in matches if word not in stop_words]
+    matches = [word for word in matches if len(word) >= 3]
+    return matches
 
-print("Completed in {} min".format((time()-start)/60))
+
+def clean_up(document):
+    cleaned = [clean_up_sentences(w) for w in document]
+    joined = [' '.join(w) for w in cleaned]
+    return joined
+
+
+def main():
+    start = time()
+    root_path = '../data/archive/'
+    spark = init_spark()
+    json_files = read_json_files(root_path, spark)
+    data = get_body_text(spark, json_files)
+
+    # clean the data
+    word_clean_up_F = F.udf(lambda x: clean_up(x), StringType())
+    data = data.withColumn("body_text_cleaned", word_clean_up_F("body_text"))
+
+    tokenizer = Tokenizer(inputCol="body_text_cleaned", outputCol="words")
+    token_DataFrame = tokenizer.transform(data)
+
+    # Remove stopwords
+    remover = StopWordsRemover(inputCol="words", outputCol="filtered")
+    cleaned_DataFrame = remover.transform(token_DataFrame)
+
+    # Count vectorizer
+    cv_tmp = CountVectorizer(inputCol="filtered", outputCol="features")
+    cvmodel = cv_tmp.fit(cleaned_DataFrame)
+    df_vect = cvmodel.transform(cleaned_DataFrame)
+
+    # Fit the LDA Model
+    num_topics = 10
+    max_iterations = 50
+    lda = LDA(seed=1, optimizer="em", k=num_topics, maxIter=max_iterations)
+    lda_model = lda.fit(df_vect)
+    print("done fitting")
+    # joblib.dump(lda_model, 'lda.csv')
+
+    # Get terms per topic
+    topics = lda_model.topicsMatrix()
+    vocabArray = cvmodel.vocabulary
+
+    wordNumbers = 15  # number of words per topic
+    topicIndices = lda_model.describeTopics(maxTermsPerTopic=wordNumbers).rdd.map(tuple)
+
+    topics_final = topicIndices.map(lambda topic: topic_render(topic, wordNumbers, vocabArray)).collect()
+
+    for topic in range(len(topics_final)):
+        print("Topic" + str(topic) + ":")
+        print(topics_final[topic])
+
+    print("Completed in {} min".format((time() - start) / 60))
+
+
+if __name__ == '__main__':
+    main()
