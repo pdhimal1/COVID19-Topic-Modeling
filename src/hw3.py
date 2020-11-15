1c16721a058b8118ac839c390e8196ee8d2cdf1b
Prakash Dhimal <dhimalp@gmail.com>
2020-11-15 16:10:24 -0500

    add better cleanup

--- src/hw3.py
+++ src/hw3.py
@@ -26,26 +26,22 @@ https://www.kaggle.com/maksimeren/covid-19-literature-clustering
 Reading this data with pyspark - done
 https://www.kaggle.com/jonathanbesomi/cord-19-sources-unification-with-pyspark-sql
 """
-
 import glob
+from time import time
 
+from nltk.corpus import stopwords
 from pyspark.ml.clustering import LDA
-from pyspark.ml.feature import CountVectorizer, Tokenizer
+from pyspark.ml.feature import CountVectorizer, Tokenizer, StopWordsRemover
 from pyspark.sql import SparkSession
 from pyspark.sql import functions as F
-from pyspark.sql.functions import monotonically_increasing_id
 from pyspark.sql.types import StringType
+
+stop_words = set(stopwords.words('english'))
 
 
 def init_spark():
-    spark = SparkSession.builder.appName("HW3-data-exploration").getOrCreate()
+    spark = SparkSession.builder.appName("HW3-Coord-data").getOrCreate()
     return spark
-
-
-def read_metadata(root_path, spark):
-    metadata_path = f'{root_path}/metadata.csv'
-    meta_df = spark.read.option("header", True).csv(metadata_path)
-    return meta_df
 
 
 def read_json_files(root_path, spark):
@@ -58,7 +54,7 @@ def read_json_files(root_path, spark):
     return data
 
 
-def get_title_abstract_text(spark, data):
+def get_body_text(spark, data):
     # Select text columns
     # todo - add more columns
     covid_sql = spark.sql(
@@ -71,44 +67,74 @@ def get_title_abstract_text(spark, data):
     return covid_sql
 
 
-def parseVectors(line):
-    return [int(line[1]), line[0]]
+def topic_render(topic, wordNumbers, vocabArray):  # specify vector id of words to actual words
+    terms = topic[1]
+    result = []
+    for i in range(wordNumbers):
+        term = vocabArray[terms[i]]
+        result.append(term)
+    return result
+
+
+def clean_up_sentences(sentence):
+    matches = [word for word in sentence.split(' ') if word.isalnum()]
+    matches = [word.lower() for word in matches]
+    matches = [word for word in matches if word not in stop_words]
+    matches = [word for word in matches if len(word) >= 3]
+    return matches
+
+
+def clean_up(document):
+    cleaned = [clean_up_sentences(w) for w in document]
+    joined = [' '.join(w) for w in cleaned]
+    return joined
 
 
 def main():
+    start = time()
     root_path = '../data/archive/'
     spark = init_spark()
     json_files = read_json_files(root_path, spark)
-    data = get_title_abstract_text(spark, json_files)
+    data = get_body_text(spark, json_files)
 
-    word_join_f = F.udf(lambda x: [''.join(w) for w in x], StringType())
-    data_df = data.withColumn("body_text", word_join_f("body_text"))
+    # clean the data
+    word_clean_up_F = F.udf(lambda x: clean_up(x), StringType())
+    data = data.withColumn("body_text", word_clean_up_F("body_text_cleaned"))
 
-    # Tokenize the text in the text column
-    tokenizer = Tokenizer(inputCol="body_text", outputCol="words")
-    wordsDataFrame = tokenizer.transform(data_df)
-    wordsDataFrame.toPandas()
+    tokenizer = Tokenizer(inputCol="body_text_cleaned", outputCol="words")
+    token_DataFrame = tokenizer.transform(data)
+
+    # Remove stopwords
+    remover = StopWordsRemover(inputCol="words", outputCol="filtered")
+    cleaned_DataFrame = remover.transform(token_DataFrame)
 
     # Count vectorizer
-    cv_tmp = CountVectorizer(inputCol="words", outputCol="features")
-    cvmodel = cv_tmp.fit(wordsDataFrame)
-    df_vect = cvmodel.transform(wordsDataFrame)
+    cv_tmp = CountVectorizer(inputCol="filtered", outputCol="features")
+    cvmodel = cv_tmp.fit(cleaned_DataFrame)
+    df_vect = cvmodel.transform(cleaned_DataFrame)
 
-    # todo - need to add id column here?
-    df_vect = df_vect.select("*").withColumn("id", monotonically_increasing_id())
-
-    # n_components=50, random_state=0
+    # Fit the LDA Model
     num_topics = 10
     max_iterations = 50
-    # Train the LDA model, set seed?
     lda = LDA(seed=1, optimizer="em", k=num_topics, maxIter=max_iterations)
-    model = lda.fit(df_vect)
+    lda_model = lda.fit(df_vect)
+    print("done fitting")
+    # joblib.dump(lda_model, 'lda.csv')
 
-    wordNumbers = 10  # number of words per topic
-    topicIndices = model.describeTopics(maxTermsPerTopic=wordNumbers)
-    topicIndices.show()
+    # Get terms per topic
+    topics = lda_model.topicsMatrix()
+    vocabArray = cvmodel.vocabulary
 
-    print(model)
+    wordNumbers = 15  # number of words per topic
+    topicIndices = lda_model.describeTopics(maxTermsPerTopic=wordNumbers).rdd.map(tuple)
+
+    topics_final = topicIndices.map(lambda topic: topic_render(topic, wordNumbers, vocabArray)).collect()
+
+    for topic in range(len(topics_final)):
+        print("Topic" + str(topic) + ":")
+        print(topics_final[topic])
+
+    print("Completed in {} min".format((time() - start) / 60))
 
 
 if __name__ == '__main__':
