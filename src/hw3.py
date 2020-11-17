3d77f5f2cad987e31d40431c0b3853c372907447
Prakash Dhimal <dhimalp@gmail.com>
2020-11-16 21:12:30 -0500

    adding 32k t5 iteration 20 [default] output

--- src/hw3.py
+++ src/hw3.py
@@ -15,7 +15,6 @@ from time import time
 
 import numpy as np
 import pyLDAvis
-from nltk import PorterStemmer
 from nltk.corpus import stopwords
 from pyspark import SparkContext
 from pyspark.ml.clustering import LDA
@@ -31,23 +30,22 @@ stop_words = set(stopwords.words('english'))
 def init_spark():
     SparkContext.setSystemProperty('spark.local.dir', '/home/dhimal/spark')
     spark = SparkSession.builder \
-        .master("local[*]") \
+        .master("local") \
         .config("spark.executor.memory", "32g") \
         .config("spark.driver.memory", "32g") \
         .config("spark.memory.offHeap.enabled", True) \
-        .config("spark.memory.offHeap.size", "32g") \
+        .config("spark.memory.offHeap.size", "100g") \
         .appName("hw3") \
         .getOrCreate()
     return spark
 
 
-def read_json_files(root_path, spark):
+def read_json_files(root_path, spark, num):
     json_dir = root_path + "document_parses/pdf_json/"
     filenames = os.listdir(json_dir)
 
     all_json = [json_dir + filename for filename in filenames]
-    # todo - for now restrict this to 100 files
-    all_json = all_json[:1000]
+    all_json = all_json[:num]
 
     data = spark.read.json(all_json, multiLine=True)
     data.createOrReplaceTempView("data")
@@ -75,11 +73,9 @@ def topic_render(topic, wordNumbers, vocabArray):  # specify vector id of words 
 
 
 def clean_up_sentences(sentence):
-    stemmer = PorterStemmer()
     matches = [word for word in sentence.split(' ') if word.isalnum()]
     matches = [word.lower() for word in matches]
     matches = [word for word in matches if word not in stop_words]
-    matches = [stemmer.stem(word) for word in matches]
     matches = [word for word in matches if len(word) >= 4]
     return matches
 
@@ -99,6 +95,27 @@ def format_data_to_pyldavis(cleaned_DataFrame, cvmodel, lda_transformed, lda_mod
     return data
 
 
+# See https://stackoverflow.com/questions/41819761/pyldavis-visualization-of-pyspark-generated-lda-model
+def filter_bad_docs(data):
+    bad = 0
+    doc_topic_dists_filtrado = []
+    doc_lengths_filtrado = []
+
+    for x, y in zip(data['doc_topic_dists'], data['doc_lengths']):
+        if np.sum(x) == 0:
+            bad += 1
+        elif np.sum(x) != 1:
+            bad += 1
+        elif np.isnan(x).any():
+            bad += 1
+        else:
+            doc_topic_dists_filtrado.append(x)
+            doc_lengths_filtrado.append(y)
+
+    data['doc_topic_dists'] = doc_topic_dists_filtrado
+    data['doc_lengths'] = doc_lengths_filtrado
+
+
 def clean_up(document):
     cleaned = [clean_up_sentences(w) for w in document]
     joined = [' '.join(w) for w in cleaned]
@@ -107,42 +124,48 @@ def clean_up(document):
 
 def main():
     timeStamp = str(int(time()))
-    out_file_name = '../out/output-' + timeStamp + '.txt'
+    # todo
+    num = 100
+    out_file_name = '../out/output-' + timeStamp + "-" + str(num) + '.txt'
     out_file = open(out_file_name, 'w')
 
     start = time()
     root_path = '../data/archive/'
     spark = init_spark()
-    json_files = read_json_files(root_path, spark)
+    json_files = read_json_files(root_path, spark, num)
     data = get_body_text(spark, json_files)
     print("data reading done")
 
     # clean the data
     word_clean_up_F = F.udf(lambda x: clean_up(x), StringType())
     data = data.withColumn("body_text_cleaned", word_clean_up_F("body_text"))
+    data = data.select("body_text_cleaned")
     print("data processing done")
 
     tokenizer = Tokenizer(inputCol="body_text_cleaned", outputCol="words")
     token_DataFrame = tokenizer.transform(data)
+    token_DataFrame = token_DataFrame.select("words")
 
     # Remove stopwords
     remover = StopWordsRemover(inputCol="words", outputCol="filtered")
     cleaned_DataFrame = remover.transform(token_DataFrame)
+    cleaned_DataFrame = cleaned_DataFrame.select('filtered')
 
     # Count vectorizer
     cv_tmp = CountVectorizer(inputCol="filtered", outputCol="count_features")
     cvmodel = cv_tmp.fit(cleaned_DataFrame)
     count_dataframe = cvmodel.transform(cleaned_DataFrame)
+    count_dataframe = count_dataframe.select('count_features')
 
     # TF-IDF Vectorizer
     tfidf = IDF(inputCol="count_features", outputCol="features")
     tfidfmodel = tfidf.fit(count_dataframe)
-    tfidf_dataframe = tfidfmodel.transform(count_dataframe)
+    tfidf_dataframe = tfidfmodel.transform(count_dataframe).select("features")
 
     print("Ready to fit with the LDA model")
     # Fit the LDA Model
-    num_topics = 10
-    max_iterations = 10
+    num_topics = 5
+    max_iterations = 20
     lda_start = time()
     lda = LDA(seed=1, optimizer="em", k=num_topics, maxIter=max_iterations)
     lda_model = lda.fit(tfidf_dataframe)
@@ -170,12 +193,23 @@ def main():
     print("LDA runtime : {} min. ".format((lda_end - lda_start) / 60))
     print("Check" + out_file.name)
 
+    # cleaned_DataFrame.write.csv('cleaned_DataFrame' + timeStamp + "-" + str(num) + '.csv')
+    # cvmodel.save('cvmodel' + timeStamp + "-" + str(num) + '.csv')
+    # lda_transformed.write.csv('lda_transformed' + timeStamp + "-" + str(num) + '.csv')
+    # lda_model.write.csv('lda_model' + timeStamp + "-" + str(num) + '.csv')
+    cleaned_DataFrame.cache()
+    lda_transformed.cache()
+
     # Data Visualization
     data = format_data_to_pyldavis(cleaned_DataFrame, cvmodel, lda_transformed, lda_model)
+    print("Preparing data with pyLDAvis ...")
+    filter_bad_docs(data)
     py_lda_prepared_data = pyLDAvis.prepare(**data)
     file_name = '../out/data-viz-' + timeStamp + '.html'
+    print("Saving pyLDAvis html page ...")
     pyLDAvis.save_html(py_lda_prepared_data, file_name)
     pyLDAvis.show(py_lda_prepared_data)
+    spark.stop()
 
 
 if __name__ == '__main__':
